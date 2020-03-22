c038c23f4c2952c519a07c8a7477ab21e6d4b08b
unknown <fads2359@gmail.com>
2020-03-21 19:32:47 -0600

    Se agrega configuración de Mongo y Handlebars

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express  = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const assert = require('assert');
+const mongoose = require('mongoose');
+require ('dotenv').config();
+
+//Se conecta a base de datos MongoDB Atlas
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser:true, 
+    useUnifiedTopology: true
+});
 
 //Borrar
 const members = require('./Members');
@@ -9,27 +18,39 @@ const members = require('./Members');
 const app = express();
 
 //Init Middleware
-app.use(logger);
+//app.use(logger);
 
 //Handlebars middleware
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({
+    extName : 'handlebars',
+    partialsDir : [
+        path.join(__dirname, 'views/partials')
+    ]
+}));
 app.set('view engine', 'handlebars');
 
 //Body parser
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
- app.get('/',(req,res) => {
-     res.render('index', {
-         tittle: 'Member app',
-         members
-     });;
- });
+app.get('/',(req,res) => {
+    console.log(__dirname);
+    res.render('index', {
+        tittle: 'Hola bianca',
+        members
+    });
+});
 
-app.use(express.static(path.join(__dirname,'public')));
+app.get('/hola',(req,res) => {
+    res.render('prueba');
+});
 
-//Members API site
+//app.use(express.static(path.join(__dirname,'public')));
+
+//Members API site www.bosersalseo.com/api/members/
 app.use('/api/members',require('./routes/api/members'));
+//Ruta para manejo de memes
+app.use('/api/imagenes',require('./routes/api/imagenes'));
 
 const PORT = process.env.PORT || 5000;
 
