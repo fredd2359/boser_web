89872b788ba508f80b509209589e5ebd8eb41b0c
unknown <fads2359@gmail.com>
2020-04-28 00:37:11 -0500

    Se agrega vuetify para estilo

---
+++ src/components/Login.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <h1>Pagina de login</h1>
+    <input type='text' name='user' v-model="user" placeholder='Usuario'>
+    <input type='password' name='password' v-model="password" placeholder='Contraseña'>
+    <button @click="login" >Iniciar Sesión</button>
+    <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+    >
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="Name"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+
+      <v-select
+        v-model="select"
+        :items="items"
+        :rules="[v => !!v || 'Item is required']"
+        label="Item"
+        required
+      ></v-select>
+
+      <v-checkbox
+        v-model="checkbox"
+        :rules="[v => !!v || 'You must agree to continue!']"
+        label="Do you agree?"
+        required
+      ></v-checkbox>
+
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="validate"
+      >
+        Validate
+      </v-btn>
+
+      <v-btn
+        color="error"
+        class="mr-4"
+        @click="reset"
+      >
+        Reset Form
+      </v-btn>
+
+      <v-btn
+        color="warning"
+        @click="resetValidation"
+      >
+        Reset Validation
+      </v-btn>
+  </v-form>
+  </div>
+</template>
+
+<script>
+import Authentication from '@/servicios/Authentication'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    async login () {
+      console.log(`Empieza el metodo ${this.user}`)
+      const response = await Authentication.register({
+        user: this.user,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
