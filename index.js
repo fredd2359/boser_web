8b00440cc0e5581a4b03fbceea48833b4f9686ea
Bianca1717 <bianka11flores@gmail.com>
2020-05-05 12:19:18 -0500

    front
     with '#' will be ignored, and an empty message aborts the commit.

---
+++ index.js
@@ -0,0 +1,64 @@
+const express  = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const assert = require('assert');
+const mongoose = require('mongoose');
+require ('dotenv').config();
+
+//Se conecta a base de datos MongoDB Atlas
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser:true, 
+    useUnifiedTopology: true
+});
+
+//Borrar
+const members = require('./Members');
+
+const app = express();
+
+//Init Middleware
+//app.use(logger);
+
+//Handlebars middleware
+app.engine('handlebars', exphbs({
+    extName : 'handlebars',
+    partialsDir : [
+        path.join(__dirname, 'views/partials')
+    ]
+}));
+app.set('view engine', 'handlebars');
+
+//Body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/',(req,res) => {
+    console.log(__dirname);
+    res.render('index', {
+        tittle: 'Hola bianca',
+        members
+    });
+});
+
+app.get('/hola',(req,res) => {
+    res.render('prueba');
+});
+
+//app.use(express.static(path.join(__dirname,'public')));
+
+//Members API site www.bosersalseo.com/api/members/
+app.use('/api/members',require('./routes/api/members'));
+//Ruta para manejo de memes
+app.use('/api/imagenes',require('./routes/api/imagenes'));
+
+//Pubic directory
+app.use('/', express.static(path.join(__dirname,'public')));
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+    console.log(path.join(__dirname,'public'));
+    console.log('Server started on port: ' + PORT);
+});
---
+++ routes/api/imagenes.js
@@ -0,0 +1,14 @@
+//Api para los memes
+const express = require('express'); 
+const router = express.Router();
+
+//Obtiene todos los memes (o los que irán en el carrousel)
+router.get('/', (req,res, next) => {
+
+});
+
+router.post('/',(req,res,next) => {
+
+});
+
+module.exports = router;
