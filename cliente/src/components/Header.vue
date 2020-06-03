5d61a3cd4789a8e013dc1c365227be5f21fea404
unknown <fads2359@gmail.com>
2020-06-02 23:33:11 -0500

    Se agrega template de Header

---
+++ cliente/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <!-- <page-header /> -->
+        <v-img class="responsive" src="http://localhost:5000/imagenes/Pportada.jpg">
+        </v-img>
+        <v-toolbar
+          dark
+          accent-4
+          dense
+        >
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+          <v-toolbar-title>
+            <img src="http://localhost:5000/imagenes/logoblanco.png" class="img1" alt="">
+          </v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn>
+            <span>
+              Hola
+            </span>
+            <!-- <v-icon>mdi-magnify</v-icon> -->
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-toolbar>
+    </div>
+</template>
+<script>
+export default {
+//   name: 'header'
+}
+</script>
+<style>
+
+</style>
