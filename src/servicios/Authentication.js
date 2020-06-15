883fd75d8556fdc9f7ee3e15d4c17d43778f7faa
unknown <fads2359@gmail.com>
2020-06-14 22:06:34 -0500

    Se agrega la funcionalidad del login conectado a back

--- src/servicios/Authentication.js
+++ src/servicios/Authentication.js
@@ -3,12 +3,20 @@ import Api from './Api'
 export default {
   login (credenciales) {
     // const credenciales2 = JSON.stringify(credenciales)
-    Api().post('/user/login', credenciales)
+    return Api().post('http://localhost:5000/user/login', credenciales)
       .then(res => {
         console.log('respuesta: ' + res)
+        return {
+          status: 201,
+          data: res.data
+        }
       })
       .catch(err => {
         console.log('Error: ' + err)
+        return {
+          status: 401,
+          data: err
+        }
       })
   }
 }
