2af29ba1283242f99ed151d9eff62754c6c07454
unknown <fads2359@gmail.com>
2020-07-12 12:03:48 -0500

    Se guarda el token que recibe del servidor

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,140 +1,148 @@
 <template>
-  <v-container column>
-    <v-row style='margin-top: 5%'>
-      <v-expansion-panels
-        :accordion=true
-        :multiple=true
-        :focusable=true
-        :hover=true
-        :value="activos"
-      >
-        <v-expansion-panel>
-          <v-expansion-panel-header>Imágenes</v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <!-- Aqui se pondrá la lista de las 10 imagenes y  -->
-            <!-- :src="require(`${card}`)" -->
-            <v-row class="d-flex justify-center">
-              <v-col
-                :cols= 6
-              >
-                <v-card>
-                  <v-img
-                    :src= "imagenSel"
-                    class="white--text align-end"
-                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                    height="200px"
-                  >
-                  <v-card-title class="justify-end">
-                    <v-btn
-                      class="mx-2"
-                      fab
-                      dark
-                      small
-                      color="pink"
-                      @click="borrarImagen"
-                    >
-                      <!-- <v-icon dark>mdi-heart</v-icon> -->
-                      <i class="far fa-trash-alt"></i>
-                    </v-btn>
-                  </v-card-title>
-                  </v-img>
-                </v-card>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col block>
-                <v-simple-table>
-                  <template v-slot:default>
-                    <thead>
-                      <tr>
-                        <th class="text-left col-nombre">Imagen</th>
-                        <th class="text-left col-fecha">Fecha de Subida</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr v-for="imagen in imagenes.imagenes" :key="imagen.name" @click="cambiarImagen(imagen)">
-                        <td class="text-left">{{ imagen.nombre }}</td>
-                        <td class="text-left">{{ imagen.fechaCreacion }}</td>
-                      </tr>
-                    </tbody>
-                  </template>
-                </v-simple-table>
-              </v-col>
-            </v-row>
-            <v-row class="">
-              <v-col class="col-12">
-                <form @submit.prevent="subirImagen" enctype="multipart/form-data">
-                  <v-file-input accept="image/*" v-model="imagenSubir" label="Subir una Imagen"></v-file-input>
-                  <v-btn
-                    color="success"
-                    dark
-                    @click="subirImagen"
-                  >
-                    Subir +
-                  </v-btn>
-                </form>
-              </v-col>
-            </v-row>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-        <v-expansion-panel>
-          <v-expansion-panel-header>Links</v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <!-- Aqui se pondrá la modificiación de links de la pagina principal  -->
-            <v-row>
-              <v-col class="col-12" v-for="link in links.links" :key="link.ruta">
-                <form>
-                  <v-row v-model="link._id" class="justify-center">
-                    <v-text-field
-                      v-model="link.ruta"
-                      label="Teclear Link"
-                    ></v-text-field>
-                    <v-btn
-                      color="primary"
-                      dark
-                      @click="editarLink(link._id)"
-                    >
-                      <i class="far fa-edit"></i>
-                    </v-btn>
-                  </v-row>
-                </form>
-              </v-col>
-            </v-row>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </v-row>
-    <v-row>
-      <v-card>
-        <v-img
-          :src="urlImg"
-          class="white--text align-end"
-          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-          height="200px"
+  <div>
+    <page-header class="width: 100%" />
+    <v-container column>
+      <v-row style='margin-top: 5%'>
+        <v-expansion-panels
+          :accordion=true
+          :multiple=true
+          :focusable=true
+          :hover=true
+          :value="activos"
         >
-        <v-card-title class="justify-end">
-          <v-btn
-            class="mx-2"
-            fab
-            dark
-            small
-            color="pink"
-            @click="borrarImagen"
+          <v-expansion-panel>
+            <v-expansion-panel-header>Imágenes</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <!-- Aqui se pondrá la lista de las 10 imagenes y  -->
+              <!-- :src="require(`${card}`)" -->
+              <v-row class="d-flex justify-center">
+                <v-col
+                  :cols= 6
+                >
+                  <v-card>
+                    <v-img
+                      :src= "imagenSel"
+                      class="white--text align-end"
+                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                      height="200px"
+                    >
+                    <v-card-title class="justify-end">
+                      <v-btn
+                        class="mx-2"
+                        fab
+                        dark
+                        small
+                        color="pink"
+                        @click="borrarImagen"
+                      >
+                        <!-- <v-icon dark>mdi-heart</v-icon> -->
+                        <i class="far fa-trash-alt"></i>
+                      </v-btn>
+                    </v-card-title>
+                    </v-img>
+                  </v-card>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col block>
+                  <v-simple-table>
+                    <template v-slot:default>
+                      <thead>
+                        <tr>
+                          <th class="text-left col-nombre">Imagen</th>
+                          <th class="text-left col-fecha">Fecha de Subida</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr v-for="imagen in imagenes.imagenes" style="cursor: pointer;" :key="imagen.name" @click="cambiarImagen(imagen)">
+                          <td class="text-left">{{ imagen.nombre }}</td>
+                          <td class="text-left">{{ imagen.fechaCreacion }}</td>
+                        </tr>
+                      </tbody>
+                    </template>
+                  </v-simple-table>
+                </v-col>
+              </v-row>
+              <v-row class="">
+                <v-col class="col-12">
+                  <form @submit.prevent="subirImagen" enctype="multipart/form-data">
+                    <v-file-input accept="image/*" v-model="imagenSubir" label="Subir una Imagen"></v-file-input>
+                    <v-btn
+                      color="success"
+                      dark
+                      @click="subirImagen"
+                    >
+                      Subir +
+                    </v-btn>
+                  </form>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+          <v-expansion-panel>
+            <v-expansion-panel-header>Links</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <!-- Aqui se pondrá la modificiación de links de la pagina principal  -->
+              <v-row>
+                <v-col class="col-12" v-for="link in links.links" :key="link.ruta">
+                  <form>
+                    <v-row v-model="link._id" class="justify-center">
+                      <v-text-field
+                        v-model="link.ruta"
+                        label="Teclear Link"
+                      ></v-text-field>
+                      <v-btn
+                        color="primary"
+                        dark
+                        @click="editarLink(link._id)"
+                      >
+                        <i class="far fa-edit"></i>
+                      </v-btn>
+                    </v-row>
+                  </form>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-row>
+      <!-- <v-row>
+        <v-card>
+          <v-img
+            :src="urlImg"
+            class="white--text align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
           >
-            <!-- <v-icon dark>mdi-heart</v-icon> -->
-            <i class="far fa-trash-alt"></i>
-          </v-btn>
-        </v-card-title>
-        </v-img>
-      </v-card>
-    </v-row>
-  </v-container>
+          <v-card-title class="justify-end">
+            <v-btn
+              class="mx-2"
+              fab
+              dark
+              small
+              color="pink"
+              @click="borrarImagen"
+            >
+              <i class="far fa-trash-alt"></i>
+            </v-btn>
+          </v-card-title>
+          </v-img>
+        </v-card>
+      </v-row> -->
+    </v-container>
+  </div>
 </template>
 
 <script>
 import Admin from '@/servicios/Admin'
 import Visuales from '@/servicios/Visuales'
+import PageHeader from '@/components/Header.vue'
+
 export default {
+  name: 'admin',
+  components: {
+    PageHeader
+  },
   data: () => ({
     activos: [0],
     imagenSel: 'http://localhost:5000/imagenes/estadio.jpg',
--- src/main.js
+++ src/main.js
@@ -5,15 +5,19 @@ import App from './App'
 import router from './router'
 import vuetify from '@/plugins/vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import { sync } from 'vuex-router-sync'
+import store from '@/store/store'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+// Vue.use(vuetify)
+sync(store, router)
+
 new Vue({
   vuetify,
   el: '#app',
   router,
-  // render: h => h(App),
+  store,
   components: { App },
   template: '<App/>'
 }).$mount('#app')
