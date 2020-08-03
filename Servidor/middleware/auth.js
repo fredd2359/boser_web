f9db407244f57c0256b39901b88407c25583898b
unknown <fads2359@gmail.com>
2020-08-02 22:17:53 -0500

    se agregan correcciones de es-lint

--- Servidor/middleware/auth.js
+++ Servidor/middleware/auth.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req,res, next) => {
     try {
-        const decoded = jwt.verify(req.body.token, process.env.JWT_SECRET);
+        // const decoded = jwt.verify(req.body.token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
         req.userData = decoded;
         next();
     }
