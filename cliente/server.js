72d69f09a72650a69c3deffd515eca6a8d2072c7
unknown <fads2359@gmail.com>
2020-08-03 01:34:49 -0500

    Se cambia servidor

--- cliente/server.js
+++ cliente/server.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var history = require('connect-history-api-fallback');
 var path = require('path');
 // var serveStatic = require('serve-static');
 app = express();
+
+app.use(history({
+  // OPTIONAL: Includes more verbose logging
+  verbose: true
+}))
+
 app.use(express.static(__dirname + "/dist"));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'index.html'));
+//   });
 var port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log('Server started on port: ' + port);
