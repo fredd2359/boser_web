e68fa07aafdc35837e4858cd72885789870da8f0
unknown <fads2359@gmail.com>
2020-11-06 01:56:08 -0600

    compilado 3

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,10 @@
 module.exports = {
   NODE_ENV: '"production"',
   VUE_APP_SERVER: '"https://salseofutbolero-server.herokuapp.com"',
+  FACEBOOK_APP: '"2824909654304731"',
+  FB_GRAPH_Link: '"https://graph.facebook.com/oauth/access_token"',
+  FB_SECRET_APP: '"64d0b1eef45c94bcfa721731110e0d69"',
+  FB_GRANT_TYPE: '"client_credentials"',
   // Cuenta Freddy
   // COSMIC_SERVER: '"https://api.cosmicjs.com/v1/boser-web-prueba/"',
   // COSMIC_READ_KEY: '"EQLso59EH3QsFHAOZs7y68nfEvh0xeKXzgNBaI7GKg6rjEePSv"',
