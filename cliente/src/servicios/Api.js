e9bfd17df1a776a173cbc0bc7a7276a1ec822980
unknown <fads2359@gmail.com>
2020-11-06 16:03:01 -0600

    Compilacion 6 npm run build

--- cliente/src/servicios/Api.js
+++ cliente/src/servicios/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseUrl: 'http://localhost:5000',
+    baseUrl: '',
     headers: {
       'Content-Type': 'application/json;charset=UTF-8'
       // 'Access-Control-Allow-Origin': '*'
