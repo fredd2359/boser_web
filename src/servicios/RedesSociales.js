a1c95db5cf31ccfc4df7b61682e94fefa935c437
unknown <fads2359@gmail.com>
2020-11-12 20:52:10 -0600

    Compilacion 11 que quita credenciales de cadena

--- src/servicios/RedesSociales.js
+++ src/servicios/RedesSociales.js
@@ -5,11 +5,15 @@ export default {
   TAccess () {
     var fbApp = process.env.FACEBOOK_APP
     var fbGraph = process.env.FB_GRAPH_Link
-    var fbSecret = process.env.FB_SECRET_APP
+    // var fbSecret = process.env.FB_SECRET_APP
     var fbGrant = process.env.FB_GRANT_TYPE
-    var cadena = '?client_id=' + fbApp + '&client_secret=' + fbSecret + '&grant_type=' + fbGrant
+    // var cadena = '?client_id=' + fbApp + '&client_secret=' + process.env.FB_SECRET_APP + '&grant_type=' + fbGrant
     // return Api().get(path.join(process.env.COSMIC_SERVER + cadena))
-    return Api().get(fbGraph + cadena)
+    return Api().get(fbGraph, {
+      client_id: fbApp,
+      client_secret: process.env.FB_SECRET_APP,
+      grant_type: fbGrant
+    })
       .then(res => {
         return {
           success: true,
