6519df91e5a219bc1535bbba535b1ab7364c4307
unknown <fads2359@gmail.com>
2021-02-03 18:49:59 -0600

    se cambia cors de axios del cliente

--- cliente/src/servicios/Api.js
+++ cliente/src/servicios/Api.js
@@ -4,8 +4,8 @@ export default () => {
   return axios.create({
     baseUrl: '',
     headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
-      // 'Access-Control-Allow-Origin': '*'
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Access-Control-Allow-Origin': '*'
     }
   })
 }
