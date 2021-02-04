e61c3ebde095f9fbe33c52d91054755137cba429
unknown <fads2359@gmail.com>
2021-02-03 18:18:12 -0600

    segundo cambio para cors en servidor

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const app = express();
 //Se activa cors
  app.use(function(req, res, next) {
      res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
      next();
  });
 
